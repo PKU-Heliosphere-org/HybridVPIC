19ac7f365cdd263b7ccf2b8483fc6ca17c362ec2
wangweining123 <120719509+wangweining123@users.noreply.github.com>
2025-02-21 11:53:31 +0800

    Update tracking_data_read.py

--- examples/WangWeining/tracking_data_read.py
+++ examples/WangWeining/tracking_data_read.py
@@ -11,10 +11,12 @@ import pyvista as pv
 
 
 class Tracer:
-    def __init__(self, tracer_name, fdir, filename):
+    def __init__(self, tracer_name, tracer_fullname, fdir, filename, sample_step=1):
         self.name = tracer_name
+        self.fullname = tracer_fullname
         self.fdir = fdir
         self.filename = filename
+        self.sample_step = sample_step
         # 初始化属性字典
         self.data = {
             'nptl': None, 'nframe': None,
@@ -34,15 +36,18 @@ class Tracer:
             fpath = os.path.join(self.fdir, self.filename)
             with h5py.File(fpath, 'r') as fh:
                 particle_tags = list(fh.keys())
-                self.data['nptl'] = len(particle_tags)
-                for iptl in range(self.data['nptl']):
+                # 计算采样后的粒子数量
+                sampled_particle_count = len(range(0, len(particle_tags), self.sample_step))
+                self.data['nptl'] = sampled_particle_count
+
+                for i, iptl in enumerate(range(0, len(particle_tags), self.sample_step)):
                     group = fh[particle_tags[iptl]]
                     dtwpe_tracer = 0.005 * 50
                     dset = group['dX']
                     nframes, = dset.shape
-                    if iptl == 0:
+                    if i == 0:
                         for key in ['x', 'y', 'z', 'ux', 'uy', 'uz', 'bx', 'by', 'bz', 'ex', 'ey', 'ez']:
-                            self.data[key] = np.zeros((self.data['nptl'], nframes))
+                            self.data[key] = np.zeros((sampled_particle_count, nframes))
                         self.data["nframe"] = nframes
                     ptl = {}
                     for dset_name in group:
@@ -55,16 +60,59 @@ class Tracer:
                                       ('ux', 'Ux'), ('uy', 'Uy'), ('uz', 'Uz'),
                                       ('bx', 'Bx'), ('by', 'By'), ('bz', 'Bz'),
                                       ('ex', 'Ex'), ('ey', 'Ey'), ('ez', 'Ez')]:
-                        self.data[key][iptl, :] = ptl[attr]
+                        self.data[key][i, :] = ptl[attr]
         except FileNotFoundError:
             print(f"文件 {self.filename} 未找到。")
         except Exception as e:
             print(f"加载数据时发生错误: {e}")
 
-    def __getattr__(self, attr):
-        if attr in self.data:
-            return self.data[attr]
-        raise AttributeError(f"'Tracer' object has no attribute '{attr}'")
+    # def read_multi_tracer_particle_data(self):
+    #     """
+    #     读取所有追踪粒子的数据，并且记录追踪粒子的总数、追踪粒子的序号、三维位置、速度信息以及电磁场数据
+    #     """
+    #     try:
+    #         fpath = os.path.join(self.fdir, self.filename)
+    #         with h5py.File(fpath, 'r') as fh:
+    #             particle_tags = list(fh.keys())
+    #             self.data['nptl'] = len(particle_tags)
+    #             for iptl in range(0, self.data['nptl'], self.sample_step):
+    #                 group = fh[particle_tags[iptl]]
+    #                 dtwpe_tracer = 0.005 * 50
+    #                 dset = group['dX']
+    #                 nframes, = dset.shape
+    #                 if iptl == 0:
+    #                     for key in ['x', 'y', 'z', 'ux', 'uy', 'uz', 'bx', 'by', 'bz', 'ex', 'ey', 'ez']:
+    #                         self.data[key] = np.zeros((self.data['nptl'], nframes))
+    #                     self.data["nframe"] = nframes
+    #                 ptl = {}
+    #                 for dset_name in group:
+    #                     dset = group[dset_name]
+    #                     ptl[dset_name] = np.zeros(dset.shape, dset.dtype)
+    #                     dset.read_direct(ptl[dset_name])
+    #                 ttracer = np.arange(0, nframes) * dtwpe_tracer
+    #                 tmin, tmax = ttracer[0], ttracer[-1]
+    #                 for key, attr in [('x', 'dX'), ('y', 'dY'), ('z', 'dZ'),
+    #                                   ('ux', 'Ux'), ('uy', 'Uy'), ('uz', 'Uz'),
+    #                                   ('bx', 'Bx'), ('by', 'By'), ('bz', 'Bz'),
+    #                                   ('ex', 'Ex'), ('ey', 'Ey'), ('ez', 'Ez')]:
+    #                     self.data[key][iptl, :] = ptl[attr]
+    #     except FileNotFoundError:
+    #         print(f"文件 {self.filename} 未找到。")
+    #     except Exception as e:
+    #         print(f"加载数据时发生错误: {e}")
+    #
+    # def __getattr__(self, attr):
+    #     if attr in self.data:
+    #         return self.data[attr]
+    #     raise AttributeError(f"'Tracer' object has no attribute '{attr}'")
+
+    def get_max_energy(self):
+        E = self.data["ux"] ** 2 + self.data["uy"] ** 2 + self.data["uz"] ** 2
+        return np.max(E[:, -1])
+
+    def get_min_energy(self):
+        E = self.data["ux"] ** 2 + self.data["uy"] ** 2 + self.data["uz"] ** 2
+        return np.min(E[:, -1])
 
     @staticmethod
     def plot_electric_force_work_histogram(*tracers, num_bins=20, color_lst):
@@ -82,22 +130,28 @@ class Tracer:
 
         # 收集所有 tracer 在三个方向的功
         for tracer in tracers:
+            if tracer.name == "ion":
+                sample_step = 2
+            elif tracer.name == "alpha":
+                sample_step = 3
+            else:
+                sample_step = 8
             ex_mat = tracer.data["ex"]
             ey_mat = tracer.data["ey"]
             ez_mat = tracer.data["ez"]
             ux_mat = tracer.data["ux"]
             uy_mat = tracer.data["uy"]
             uz_mat = tracer.data["uz"]
-            work_x = np.sum(ex_mat * ux_mat, axis=1)
-            work_y = np.sum(ey_mat * uy_mat, axis=1)
-            work_z = np.sum(ez_mat * uz_mat, axis=1)
+            work_x = np.sum(ex_mat * ux_mat, axis=1)/tracer.get_max_energy()
+            work_y = np.sum(ey_mat * uy_mat, axis=1)/tracer.get_max_energy()
+            work_z = np.sum(ez_mat * uz_mat, axis=1)/tracer.get_max_energy()
 
             all_work_x.extend(work_x)
             all_work_y.extend(work_y)
             all_work_z.extend(work_z)
 
         all_works = [all_work_x, all_work_y, all_work_z]
-        labels_axis = [r"$W_x$", r"$W_y$", r"$W_z$"]
+        labels_axis = [r"$W_x/W_{\mathrm{max}}$", r"$W_y/W_{\mathrm{max}}$", r"$W_z/W_{\mathrm{max}}$"]
         axis_lst = ["x", "y", "z"]
         legend_handles = []  # 用于存储图例句柄
         legend_labels = []  # 用于存储图例标签
@@ -112,18 +166,16 @@ class Tracer:
             # 计算统一的 bin 边界
             bin_edges = np.linspace(min_val, max_val, num_bins + 1)
 
-            text_y_spacing = 200  # 计算每个文本之间的垂直间距
-
-
+            text_y_spacing = 300  # 计算每个文本之间的垂直间距
 
             for j, tracer in enumerate(tracers):
                 if i == 0:
-                    work_per_tracer = np.sum(tracer.data["ex"] * tracer.data["ux"], axis=1)
+                    work_per_tracer = np.sum(tracer.data["ex"] * tracer.data["ux"], axis=1)/tracer.get_max_energy()
                 elif i == 1:
-                    work_per_tracer = np.sum(tracer.data["ey"] * tracer.data["uy"], axis=1)
+                    work_per_tracer = np.sum(tracer.data["ey"] * tracer.data["uy"], axis=1)/tracer.get_max_energy()
                 else:
-                    work_per_tracer = np.sum(tracer.data["ez"] * tracer.data["uz"], axis=1)
-                hist = ax.hist(work_per_tracer, bins=bin_edges, label=tracer.name, alpha=0.5, color=color_lst[j],
+                    work_per_tracer = np.sum(tracer.data["ez"] * tracer.data["uz"], axis=1)/tracer.get_max_energy()
+                hist = ax.hist(work_per_tracer, bins=bin_edges, label=tracer.name, alpha=0.4, color=color_lst[j],
                         edgecolor='black', linewidth=0.5)
                 if i == 0:  # 只在第一个子图中收集图例句柄和标签
                     proxy_patch = Patch(facecolor=color_lst[j], edgecolor='black', label=tracer.name)
@@ -140,7 +192,7 @@ class Tracer:
                 start_y = 600  # 起始垂直位置
                 text_y = start_y + j * text_y_spacing  # 计算当前文本的垂直位置
                 xlim = ax.get_xlim()
-                ax.text(xlim[1]*0.4, text_y, fr"Mean($W_{{{axis_lst[i]},\mathrm{{{tracer.name}}}}}$)={mean_work:.2f}", fontsize=15, ha='left',
+                ax.text(xlim[1]*0.35, text_y, fr"Mean($W_{{{axis_lst[i]},\mathrm{{{tracer.name}}}}}$)={mean_work:.2f}", fontsize=15, ha='left',
                         color=color_lst[j])
 
             ax.set_xlabel(label, fontsize=15)
@@ -150,7 +202,7 @@ class Tracer:
             # ax.legend(loc='upper right')
 
         # 修改主标题
-        plt.suptitle("Electric work histogram for different particle species", fontsize=16)  # 修改为更具描述性的标题
+        plt.suptitle(" Normalized electric work histogram for different particle species", fontsize=16)  # 修改为更具描述性的标题
 
         # 添加浅色背景
         fig.patch.set_facecolor('#f9f9f9')
@@ -190,7 +242,7 @@ class Tracer:
         for i_frame in range(1, nframe):
             v_threshold = np.max(self.data["ux"][:, i_frame])*0.75
             ey_threshold = 0.7*np.max(self.data["ey"][:, i_frame])+(1-0.7)*np.min(self.data["ey"][:, i_frame])
-            print(f"step={i_frame}, Threshold velocity={v_threshold}, Threshold ey={ey_threshold}")
+            # print(f"step={i_frame}, Threshold velocity={v_threshold}, Threshold ey={ey_threshold}")
             ux = self.data["ux"][:, i_frame]
             ey = self.data["ey"][:, i_frame]
             x = self.data["x"][:, i_frame]
@@ -206,7 +258,7 @@ class Tracer:
         nframe = self.data["nframe"]
         epoch = range(nframe)
         x_shock_arr = self.x_shock_arr()
-        slope, intercept = np.polyfit(epoch, x_shock_arr, 1)
+        slope, intercept = np.polyfit(epoch[0:200], x_shock_arr[0:200], 1)
         return slope*epoch+intercept
 
     @staticmethod
@@ -256,7 +308,7 @@ class Tracer:
             cbar = plt.colorbar(sc, ax=ax)
             cbar.set_label("Epoch", fontsize=15)
             ax.set_xlabel("x(from shock)", fontsize=15)
-            ax.set_ylabel(r"$W_x$", fontsize=15)
+            ax.set_ylabel(r"$W_x", fontsize=15)
             ax = axes[3]
             sc = ax.scatter(x - x_shock, np.cumsum(uz * ez), c=range(nframe), cmap="jet")
             ax.plot(x - x_shock, np.cumsum(uz * ez), c="k")
@@ -264,7 +316,7 @@ class Tracer:
             cbar = plt.colorbar(sc, ax=ax)
             cbar.set_label("Epoch", fontsize=15)
             ax.set_xlabel("x(from shock)", fontsize=15)
-            ax.set_ylabel(r"$W_z$", fontsize=15)
+            ax.set_ylabel(r"$W_z", fontsize=15)
         plt.show()
 
     def plot_electric_work_DifferentRegion(self, xc, turbulence=False, turbulence_amplitude=None):
@@ -292,14 +344,14 @@ class Tracer:
             w_far_shock_arr[iptl] = np.sum(w_total[condition_far_shock])
             # print(f"iptl={iptl}, w_near_shock={np.sum(w_total[condition_near_shock])}")
         fig, ax = plt.subplots(1, 1, figsize=(10, 6))
-        ax.hist(w_near_shock_arr, label="near", alpha=0.5, color="r")
-        ax.hist(w_far_shock_arr, label="far", alpha=0.5, color="b")
-        ax.set_ylim([0, 1200])
+        n1, bins, patches = ax.hist(w_near_shock_arr, label="near", alpha=0.5, color="r")
+        n2, bins, patches = ax.hist(w_far_shock_arr, label="far", alpha=0.5, color="b")
+        ax.set_ylim([0, max(np.max(n1), np.max(n2))+100])
         ax.set_ylabel("Counts", fontsize=15)
         ax.set_xlabel(r"$W_{q}$", fontsize=15)
-        ax.text(w_near_shock_arr.mean(), 500, fr"$\mathrm{{Mean}}(|x-x_{{\mathrm{{shock}}}}|<{xc})={w_near_shock_arr.mean():.2f}$"
+        ax.text(w_near_shock_arr.mean()+5, 500, fr"$\mathrm{{Mean}}(|x-x_{{\mathrm{{shock}}}}|<{xc})={w_near_shock_arr.mean():.2f}$"
                 , fontsize=15, color="r")
-        ax.text(w_far_shock_arr.mean(), 700, fr"$\mathrm{{Mean}}(x-x_{{\mathrm{{shock}}}}<-{xc})={w_far_shock_arr.mean():.2f}$"
+        ax.text(w_far_shock_arr.mean()+5, 700, fr"$\mathrm{{Mean}}(x-x_{{\mathrm{{shock}}}}<-{xc})={w_far_shock_arr.mean():.2f}$"
                 , fontsize=15, color="b")
         if turbulence:
             if turbulence_amplitude is not None:
@@ -309,11 +361,22 @@ class Tracer:
         else:
             title_suffix = "\n(No Turbulence)"
 
-        ax.set_title(f"Histogram of the electric work to {self.name}s in different shock regions {title_suffix}",
+        ax.set_title(f"Histogram of the electric work to {self.fullname} in different shock regions {title_suffix}",
                      fontsize=15)
         plt.legend()
-        plt.legend()
         plt.show()
+
+    def num_particles_of_shock_crossing(self):
+        x_shock_fit = self.x_shock_arr_fit()
+        num = 0
+        nptl = self.data["nptl"]
+        x = self.data["x"]
+        for iptl in range(nptl):
+            if x[iptl, -1] < x_shock_fit[-1]:
+                num = num + 1
+        return num
+
+
 
 
 
@@ -331,28 +394,44 @@ if __name__ == "__main__":
     num_particle_traj = 2000
     ratio_emax = 1
     species_name_lst = ["ion", "alpha", "pui"]
+    sample_lst = [2, 3, 8]
 
     fname1 = f"{species_name_lst[2]}_trace_data/{species_name_lst[2]}s_ntraj{num_particle_traj}_{ratio_emax}emax_turb_amp_0dot36.h5p"
     fname2 = f"{species_name_lst[2]}_trace_data/{species_name_lst[2]}s_ntraj{num_particle_traj}_{ratio_emax}emax_turb_amp_0dot18.h5p"
-    fname3 = f"{species_name_lst[2]}_trace_data/{species_name_lst[2]}s_ntraj{num_particle_traj}_{ratio_emax}emax.h5p"
-    fname4 = f"{species_name_lst[0]}_trace_data/{species_name_lst[0]}s_ntraj{num_particle_traj}_{ratio_emax}emax.h5p"
-    fname5 = f"{species_name_lst[1]}_trace_data/{species_name_lst[1]}s_ntraj{num_particle_traj}_{ratio_emax}emax.h5p"
+    fname3 = f"{species_name_lst[2]}_trace_data/{species_name_lst[2]}s_ntraj{num_particle_traj}_{ratio_emax}emax(step=20000).h5p"
+    fname4 = f"{species_name_lst[0]}_trace_data/{species_name_lst[0]}s_ntraj8000_{ratio_emax}emax.h5p"
+    fname5 = f"{species_name_lst[1]}_trace_data/{species_name_lst[1]}s_ntraj12000_{ratio_emax}emax.h5p"
+    fname6 = f"{species_name_lst[2]}_trace_data/{species_name_lst[2]}s_ntraj32000_{ratio_emax}emax.h5p"
     fdir = f"D:/Research/Codes/Hybrid-vpic/data_ip_shock/trace_data/"
-    pui_tracer_1 = Tracer(species_name_lst[2], fdir, fname1)
-    pui_tracer_2 = Tracer(species_name_lst[2], fdir, fname2)
-    pui_tracer_3 = Tracer(species_name_lst[2], fdir, fname3)
-    ion_tracer = Tracer(species_name_lst[0], fdir, fname4)
-    alpha_tracer = Tracer(species_name_lst[1], fdir, fname5)
-    Tracer.plot_electric_force_work_histogram(pui_tracer_3, ion_tracer, alpha_tracer, color_lst=['#3498db', '#e74c3c', 'g'])
+    pui_tracer_1 = Tracer(species_name_lst[2], "PUI", fdir, fname1, sample_step=sample_lst[2])
+    pui_tracer_2 = Tracer(species_name_lst[2], "PUI", fdir, fname2, sample_step=sample_lst[2])
+    pui_tracer_3 = Tracer(species_name_lst[2], "PUI", fdir, fname3, sample_step=sample_lst[2])
+    pui_tracer_4 = Tracer(species_name_lst[2], "PUI", fdir, fname6, sample_step=sample_lst[2])
+    ion_tracer = Tracer(species_name_lst[0], r"SWI($\mathrm{H}^+$)", fdir, fname4, sample_step=sample_lst[0])
+    alpha_tracer = Tracer(species_name_lst[1], r"SWI($\mathrm{He}^{2+}$)", fdir, fname5, sample_step=sample_lst[1])
+#%%
+    # Wy = np.sum(ion_tracer.data["uy"]*ion_tracer.data["ey"], axis=1)/ion_tracer.get_max_energy()
+    # plt.hist(Wy)
+    # plt.show()
+    Tracer.plot_electric_force_work_histogram(pui_tracer_4, ion_tracer, alpha_tracer, color_lst=['#3498db', '#e74c3c', 'g'])
     # Tracer.plot_energy_variation(pui_tracer_1, pui_tracer_2, pui_tracer_3,
     #                              iptl_list=[1, 1, 2])
     # x_shock = pui_tracer_3.x_shock_arr()
-    # Tracer.plot_energy_variation_ShockFrame(pui_tracer_2, iptl_list=[1000])
+    # Tracer.plot_energy_variation_ShockFrame(ion_tracer, iptl_list=[1000])
+    # E = ion_tracer.data["ux"]**2+ion_tracer.data["uy"]**2+ion_tracer.data["uz"]**2
+    # E = pui_tracer_3.data["ux"] ** 2 + pui_tracer_3.data["uy"] ** 2 + pui_tracer_3.data["uz"] ** 2
+    # print(np.max(E[:, -1]))
+    # print(alpha_tracer.num_particles_of_shock_crossing())
     # plt.scatter(pui_tracer_2.data["x"][:, 200], pui_tracer_2.data["ey"][:, 200])
-    # alpha_tracer.plot_electric_work_DifferentRegion(xc=10)
+    print([pui_tracer_4.get_min_energy(), pui_tracer_4.get_max_energy()])
+    # ion_tracer.plot_electric_work_DifferentRegion(xc=10)
+    plt.hist(np.sum(pui_tracer_4.data["uy"]*pui_tracer_4.data["ey"]/pui_tracer_4.get_max_energy(), axis=1))
+    print(np.mean(np.sum(pui_tracer_4.data["uy"]*pui_tracer_4.data["ey"]/pui_tracer_4.get_max_energy(), axis=1)))
+    plt.show()
     # pui_tracer_2.plot_electric_work_DifferentRegion(xc=10)
     # pui_tracer_2.plot_electric_work_DifferentRegion(xc=10, turbulence=True, turbulence_amplitude=0.18)
-    # plt.scatter(range(ion_tracer.data["nframe"]), ion_tracer.x_shock_arr_fit())
+    # plt.scatter(range(ion_tracer.data["nframe"]), alpha_tracer.x_shock_arr_fit())
+    # plt.scatter(range(ion_tracer.data["nframe"]), alpha_tracer.x_shock_arr())
     # plt.xlabel("Epoch", fontsize=15)
     # plt.ylabel(r"$x_{\mathrm{shock}}$", fontsize=15)
     # plt.show()
